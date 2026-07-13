e7609c02edb616f3fd77e45aec1b5fa85fba3520
shoebmemon <137752742+shoebmemon@users.noreply.github.com>
2026-07-13 10:13:23 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "shop-stock-order-app-v135";
+const CACHE_NAME = "shop-stock-order-app-v136";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=33",
-  "./app.js?v=72",
+  "./styles.css?v=34",
+  "./app.js?v=73",
   "./manifest.webmanifest",
   "./icon.svg",
   "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/umd/supabase.min.js"
